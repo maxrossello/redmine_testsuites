b150efc5999e9533bc937310719975f2a87b33b9
Massimo Rossello <massimo.rossello@azcomtech.com>
2020-04-13 17:33:48 +0200

    Merged testsuite 4.0.7

--- init.rb
+++ init.rb
@@ -30,10 +30,10 @@ if ENV["RAILS_ENV"] == "test"
     description 'Allows to run the Redmine test suite along with plugin tests, considering the different behaviors 
                  introduced by supported plugins over the Redmine default behavior. 
                  Unsupported plugins are signaled in the logs.'
-    version '4.1.0.1'
+    version '4.1.1'
     url 'https://github.com/maxrossello/redmine_testsuites.git'
     author_url 'https://github.com/maxrossello'
-    requires_redmine :version => '4.1.0'
+    requires_redmine :version => '4.1.1'
   end 
 
   #Rails.application.config.active_support.test_order = :sorted
@@ -46,7 +46,7 @@ if ENV["RAILS_ENV"] == "test"
     redmine_better_overview:   { tilde_greater_than: '4.1.0' },
     redmine_pluggable_themes:  { tilde_greater_than: '4.1.0' },
     redmine_themes:            { tilde_greater_than: '4.1.0' },
-    redwine:                   { tilde_greater_than: '4.1.0' },
+    redwine:                   { tilde_greater_than: '4.1.1' },
     sidebar_hide:              { version_or_higher: '1.0.0' }
   }
   
