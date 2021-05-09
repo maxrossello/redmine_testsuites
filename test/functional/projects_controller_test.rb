9b8a8247ac9e71fe0f47a5b51f01d7bb5cd9934f
Massimo Rossello <counseling.mr@gmail.com>
2021-05-09 13:58:11 +0200

    Removed some dup test

--- test/functional/projects_controller_test.rb
+++ test/functional/projects_controller_test.rb
@@ -1072,17 +1072,6 @@ class ProjectsControllerTest < Redmine::ControllerTest
     assert_select '.warning', :text => /#{I18n.t(:text_project_destroy_confirmation)}/
   end
 
-  def test_destroy_leaf_project_with_wrong_confirmation_should_show_confirmation
-    @request.session[:user_id] = 1 # admin
-
-    assert_no_difference 'Project.count' do
-      delete(:destroy, :params => {:id => 2, :confirm => 'wrong'})
-      assert_response :success
-    end
-    #assert_select '.warning', :text => /Are you sure you want to delete this project/
-    assert_select '.warning', :text => /#{I18n.t(:text_project_destroy_confirmation)}/
-  end
-
   def test_destroy_without_confirmation_should_show_confirmation_with_subprojects
     set_tmp_attachments_directory
     @request.session[:user_id] = 1 # admin
