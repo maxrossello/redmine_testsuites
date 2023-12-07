4d045541ae9198e854f24b41b037e9de34be167d
Massimo Rossello <counseling.mr@gmail.com>
2023-12-07 17:32:38 +0100

    Merged 5.1.1 tests

--- test/unit/lib/redmine/wiki_formatting/html_sanitizer_test.rb
+++ test/unit/lib/redmine/wiki_formatting/html_sanitizer_test.rb
@@ -2,6 +2,12 @@
 
 # Redmine - project management software
 # Copyright (C) 2006-2023  Jean-Philippe Lang
+<<<<<<<< HEAD:test/unit/lib/redmine/wiki_formatting/html_sanitizer_test.rb
+========
+#
+# FileSystem adapter
+# File written by Paul Rivier, at Demotera.
+>>>>>>>> 5.1.1:app/models/repository/filesystem.rb
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -31,8 +37,25 @@ class Redmine::WikiFormatting::HtmlSanitizerTest < ActiveSupport::TestCase
     end
   end
 
+<<<<<<<< HEAD:test/unit/lib/redmine/wiki_formatting/html_sanitizer_test.rb
   def test_should_reject_links_with_unsafe_url_schemes
     input = %(<a href="javascript:alert('hello');">foo</a>)
     assert_equal "<a>foo</a>", @sanitizer.call(input)
+========
+  def self.scm_adapter_class
+    Redmine::Scm::Adapters::FilesystemAdapter
+  end
+
+  def self.scm_name
+    'Filesystem'
+  end
+
+  def supports_history?
+    false
+  end
+
+  def fetch_changesets
+    nil
+>>>>>>>> 5.1.1:app/models/repository/filesystem.rb
   end
 end
