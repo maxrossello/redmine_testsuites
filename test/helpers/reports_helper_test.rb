1fed831cdf7a78ddf18a884bb9dc87d37f130659
Massimo Rossello <counseling.mr@gmail.com>
2026-06-16 18:07:36 +0200

    Merged 5.1.13 tests

--- test/helpers/reports_helper_test.rb
+++ test/helpers/reports_helper_test.rb
@@ -17,23 +17,60 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-require_relative '../test_helper'
+class MailHandlerController < ActionController::Base
+  include ActiveSupport::SecurityUtils
 
-class ReportsHlperTest < Redmine::HelperTest
-  include ReportsHelper
-  include Rails.application.routes.url_helpers
+  before_action :check_credential
 
-  def test_aggregate_path_for_spacified_row
-    project = Project.find(1)
-    field = 'assigned_to_id'
-    row = User.find(2)
-    assert_equal '/projects/ecookbook/issues?assigned_to_id=2&set_filter=1&subproject_id=%21%2A', aggregate_path(project, field, row)
+  # Requests from rdm-mailhandler.rb don't contain CSRF tokens
+  skip_before_action :verify_authenticity_token
+
+  # Displays the email submission form
+  def new
   end
 
-  def test_aggregate_path_for_unset_row
-    project = Project.find(1)
-    field = 'assigned_to_id'
-    row = User.new
-    assert_equal '/projects/ecookbook/issues?assigned_to_id=%21%2A&set_filter=1&subproject_id=%21%2A', aggregate_path(project, field, row)
+  # Submits an incoming email to MailHandler
+  def index
+    # MailHandlerController#index should permit all options set by
+    # RedmineMailHandler#submit in rdm-mailhandler.rb.
+    # It must be kept in sync.
+    options = params.permit(
+      :key,
+      :email,
+      :allow_override,
+      :unknown_user,
+      :default_group,
+      :no_account_notice,
+      :no_notification,
+      :no_permission_check,
+      :project_from_subaddress,
+      {
+        issue: [
+          :project,
+          :status,
+          :tracker,
+          :category,
+          :priority,
+          :assigned_to,
+          :fixed_version,
+          :is_private
+        ]
+      }
+    ).to_h
+    email = options.delete(:email)
+    if MailHandler.safe_receive(email, options)
+      head :created
+    else
+      head :unprocessable_entity
+    end
+  end
+
+  private
+
+  def check_credential
+    User.current = nil
+    unless Setting.mail_handler_api_enabled? && secure_compare(params[:key].to_s, Setting.mail_handler_api_key.to_s)
+      render :plain => 'Access denied. Incoming emails WS is disabled or key is invalid.', :status => 403
+    end
   end
 end
--- test/system/query_test.rb
+++ test/system/query_test.rb
@@ -23,11 +23,63 @@ class QuerySystemTest < ApplicationSystemTestCase
   def test_query_filter_row_should_escape_html_elements
     cf = IssueCustomField.create!(name: 'My <select>', field_format: 'string', is_filter: true)
 
-    log_user('jsmith', 'jsmith')
-    visit '/issues'
-    # click_on 'Add filter'
-    select 'My <select>', from: 'Add filter'
+  def self.default(project=nil)
+    default_activity = super()
+
+    if default_activity.nil? || project.nil? || project.activities.blank? || project.activities.include?(default_activity)
+      return default_activity
+    end
+
+    project.activities.detect { |activity| activity.parent_id == default_activity.id }
+  end
+
+  # Returns the available activities for the time entry
+  def self.available_activities(project=nil)
+    if project.nil?
+      TimeEntryActivity.shared.active
+    else
+      project.activities
+    end
+  end
+
+  def option_name
+    OptionName
+  end
 
     assert_selector "div#tr_cf_#{cf.id} label", text: 'My <select>'
+  end
+
+  def self.default_activity_id(user=nil, project=nil)
+    default_activities = []
+    default_activity = nil
+    available_activities = self.available_activities(project)
+
+    if project && user
+      user_membership = user.membership(project)
+      if user_membership
+        default_activities = user_membership.roles.where.not(:default_time_entry_activity_id => nil).sort.pluck(:default_time_entry_activity_id)
+      end
+
+      project_default_activity = self.default(project)
+      if project_default_activity && !default_activities.include?(project_default_activity.id)
+        default_activities << project_default_activity.id
+      end
+    end
+
+    global_activity = self.default
+    if global_activity && !default_activities.include?(global_activity.id)
+      default_activities << global_activity.id
+    end
+
+    if available_activities.count == 1 && !default_activities.include?(available_activities.first.id)
+      default_activities << available_activities.first.id
+    end
+
+    default_activities.each do |id|
+      default_activity = available_activities.detect{ |a| a.id == id || a.parent_id == id }
+      break unless default_activity.nil?
+    end
+
+    default_activity&.id
   end
 end
